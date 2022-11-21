b9a8ad97dda4786a809cb7c12c8159a3fbc28e04
Enrique <0208738@up.edu.mx>
2022-11-20 22:03:33 -0600

    Aun no sirve pero es la base mas o menos

--- Data.js
+++ Data.js
@@ -1,3 +1,7 @@
+const FS = require('./firebase');
+const { dt } = FS;
+
+
 const db = [
     { "id" : 1, "email": "ejemplo@hotmail.com", "password" : "12345"},
     { "id" : 2, "email": "patito@gmail.com", "password" : "abcde" },
@@ -5,5 +9,9 @@ const db = [
     { "id" : 4, "email": "ejecutivo@empresa.com", "password" : "ceo" },
     { "id" : 5, "email": "alumno@up.com.mx", "password" : "contraseña" }
 ]
+
+const DC = dt.collection('dcollections');
+
+DC.add(db);
 
 module.exports = { db }
