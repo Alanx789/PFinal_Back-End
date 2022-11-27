5c7ccb73b0682cb6cbbe761d9df35e75d2f43624
Maria Fernanda Ojeda Carrillo <0228660@up.edu.mx>
2022-11-26 18:20:13 -0600

    some corrections added

--- Data.js
+++ Data.js
@@ -1,9 +1,9 @@
 const db = [
-    { "id" : 1, "email": "ejemplo@hotmail.com", "password" : "12345", 'name':"John"},
-    { "id" : 2, "email": "patito@gmail.com", "password" : "abcde", 'name':"Abby" },
-    { "id" : 3, "email": "correo@yahoo.mx", "password" : "correo", 'name':"Constance" },
-    { "id" : 4, "email": "ejecutivo@empresa.com", "password" : "ceo", 'name':"Mike" },
-    { "id" : 5, "email": "alumno@up.com.mx", "password" : "contraseña", 'name':"Stephen" }
+    { "id" : 1, "email": "ejemplo@hotmail.com", "password" : "12345", 'name':"John", "phone": "1457812453"},
+    { "id" : 2, "email": "patito@gmail.com", "password" : "abcde", 'name':"Abby", "phone": "8547952148" },
+    { "id" : 3, "email": "correo@yahoo.mx", "password" : "correo", 'name':"Constance", "phone": "5216342874"},
+    { "id" : 4, "email": "ejecutivo@empresa.com", "password" : "ceo", 'name':"Mike", "phone": "9648751234"},
+    { "id" : 5, "email": "alumno@up.com.mx", "password" : "contraseña", 'name':"Stephen", "phone": "9648752120"}
 ]
 
 module.exports = { db }
